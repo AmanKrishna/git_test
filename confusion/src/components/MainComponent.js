d8b52c7b9d0a4e408778940edc9fe7bfe16184d9
AmanKrishna <mnkrishn@gmail.com>
2020-06-15 16:48:09 +0530

    React Router

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -3,8 +3,10 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponents';
 import DishdetailComponents from './DishdetailComponents';
 import {DISHES} from '../shared/dishes';
-import Header from './HeaderComponent'
-import Footer from './FooterComponent'
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends React.Component {
   constructor(props) {
@@ -23,19 +25,20 @@ class Main extends React.Component {
 
   render(){
     console.log("asdasdas",typeof(onDishSelect));
+    const HomePage = ()=>{
+      return(
+        <Home/>
+      );
+    }
     return (
       <div>
         <Header/>
-        {/* defining onClick Function, the below function is same as function name(dishId){return this.onDishSelect(dishid);} */}    
-        <Menu dishes={this.state.dishes}
-            onClick={(dishId)=>this.onDishSelect(dishId)}/>
-            {/* Here I am retrieving the dish from the dish object
-             using the dish. filter allows me to iterate over
-             all the dishes in the this.state.dishes and returns 
-             an array. Therefore the [0] at the end to extract the 
-             dish*/}
-        <DishdetailComponents  currDish={this.state.dishes.filter(
-            (dish) => dish.id===this.state.selectedDish)[0]}/>
+        <Switch>
+          <Route path="/home" component={HomePage}/>
+          {/* If i want to pass props to a component */}
+          <Route path="/menu" component={()=> <Menu dishes={this.state.dishes}/>}/>
+          <Redirect to="/home"/>
+        </Switch>
         <Footer/>
       </div>
     );
